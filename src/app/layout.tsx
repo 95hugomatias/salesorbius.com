a2bb27a28f8fba2ab7217c6cdef6b988d7ee2482
95hugomatias <164529013+95hugomatias@users.noreply.github.com>
2026-09-24 12:34:09 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -89,17 +89,46 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" className={spaceGrotesk.variable}>
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),
+                  dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PRQ5W9XG');
+          `}
+        </Script>
+      </head>
+
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PRQ5W9XG"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
+
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify(organizationJsonLd),
           }}
         />
+
         <Script
           src="https://t.contentsquare.net/uxa/96bc475b676d3.js"
           strategy="afterInteractive"
         />
+
         {children}
       </body>
     </html>
